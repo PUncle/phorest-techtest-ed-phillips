30335f52e4d19868b23778c621d33bf1f9cccf18
Ed Phillips <ed@paperuncle.com>
2019-09-03 13:54:41 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 // See default config https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 module.exports = {
   theme: {
-    extend: {}
+    extend: {},
+    container: {
+      center: false
+    }
   },
   variants: {}
 }
