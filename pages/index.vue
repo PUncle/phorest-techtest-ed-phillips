68c9b23ab76e2923cb02054dfa9c12b7dad50718
Ed Phillips <ed@paperuncle.com>
2019-09-05 14:10:54 +0100

    index component

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,31 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        phorest-techtest-ed-phillips
-      </h1>
-      <h2 class="subtitle">
-        Phorest techtest
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
+  <section class="w-full">
+    <div class="container">
+      <div class="max-w-md m-auto text-center">
+        <h1 class="text-4xl">{{ message }}</h1>
+        <p>Ed Phillips</p>
+        <nuxt-link
+          class="bg-blue-500 block text-white font-bold py-2 px-4 mt-6 uppercase"
+          to="/client-search"
         >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
+          Start
+        </nuxt-link>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  head: {
+    title: 'Home page'
+  },
+  data() {
+    return {
+      message: 'Phorest tech test'
+    }
   }
 }
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style></style>
