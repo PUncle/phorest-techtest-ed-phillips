4927ec2dbe0187fd7bea4f05206bf58bd346e081
Ed Phillips <ed@paperuncle.com>
2019-09-05 14:11:21 +0100

    add axios post config object

--- plugins/axios.js
+++ plugins/axios.js
@@ -8,3 +8,12 @@ export const axiosGetData = axios.create({
     password: process.env.BASIC_AUTH_PASS
   }
 })
+
+export const axiosPostData = axios.create({
+  method: 'post',
+  baseURL: process.env.API_BASE,
+  auth: {
+    username: process.env.BASIC_AUTH_USER,
+    password: process.env.BASIC_AUTH_PASS
+  }
+})
