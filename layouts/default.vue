46522e7f8f49bbbbc88ac1c4772d00dd453b4554
Ed Phillips <ed@paperuncle.com>
2019-09-05 14:10:01 +0100

    default layout and global styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,29 @@
 <template>
-  <div>
-    <nuxt />
+  <div class="flex flex-col content-start justify-start min-h-screen font-body">
+    <Navbar class="w-full bg-gray-900 p-4" />
+    <div class="container">
+      <nuxt />
+    </div>
   </div>
 </template>
+<script>
+import Navbar from '~/components/Navbar.vue'
 
+export default {
+  components: {
+    Navbar
+  },
+  data() {
+    return {
+      loggedIn: false,
+      loading: false,
+      selectedBranchName: ''
+    }
+  }
+}
+</script>
 <style>
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -17,10 +33,47 @@ html {
   box-sizing: border-box;
 }
 
+body {
+  @apply text-gray-800;
+}
+
 *,
 *:before,
 *:after {
   box-sizing: border-box;
   margin: 0;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  @apply font-title;
+}
+
+.container {
+  @apply flex;
+  @apply justify-center;
+  @apply mx-auto;
+  @apply mt-10;
+}
+
+article {
+  @apply w-full;
+  @apply mb-4;
+  @apply border-gray-400;
+  @apply bg-white;
+  @apply rounded-b-lg;
+}
+
+article .client-status {
+  @apply text-white;
+  @apply text-xs;
+  @apply font-bold;
+  @apply py-1;
+  @apply px-4;
+  @apply uppercase;
 }
 </style>
