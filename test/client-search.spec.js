841dd4a5c5be535c377a9f11538a3965a18d1d9c
Ed Phillips <ed@paperuncle.com>
2019-09-06 11:42:59 +0100

    additional tests

--- test/client-search.spec.js
+++ test/client-search.spec.js
@@ -1,28 +1,43 @@
 import { shallowMount } from '@vue/test-utils'
 import ClientSearch from '@/pages/client-search.vue'
 
-const $route = {
-  query: {
-    term: ''
-  }
-}
-
-const factory = propsData => {
-  return shallowMount(ClientSearch, {
-    propsData: {
-      ...propsData
-    },
-    mocks: {
-      $route
-    }
-  })
-}
+let wrapper
+afterEach(() => {
+  wrapper.destroy()
+})
 
 describe('ClientSearch detail page', () => {
   it('is a Vue instance', () => {
-    const wrapper = factory(ClientSearch, {
-      something: 'whatever'
+    wrapper = shallowMount(ClientSearch, {
+      propsData: {},
+      mocks: { $route: { query: { term: '' } } },
+      stubs: {},
+      methods: {},
+      data() {
+        return {
+          formErrors: ['Error 1', 'Error 2'],
+          apiError: false,
+          apiErrorMessage: 'Oops',
+          apiLoading: false,
+          clients: null,
+          rawUserSearchInput: '',
+          validatedSearchTerm: ''
+        }
+      }
     })
-    expect(wrapper.isVueInstance()).toBeTruthy()
+    expect(wrapper.isVueInstance).toBeTruthy()
+  })
+
+  it('has 2 form errors', () => {
+    wrapper = shallowMount(ClientSearch, {
+      mocks: { $route: { query: { term: '' } } },
+      data() {
+        return {
+          formErrors: ['Error 1', 'Error 2']
+        }
+      }
+    })
+
+    expect(wrapper.findAll('.form-errors li')).toHaveLength(2)
   })
 })
